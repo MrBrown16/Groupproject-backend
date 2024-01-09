303086560a40e00468e849d0c10c611a66130c17
Balint0429 <balint02@gmail.com>
2024-01-09 16:50:59 +0100

    opening hours beggining added

---
+++ src/main/java/hu/project/groupproject/entities/OpeningHoursUsual.java
@@ -0,0 +1,12 @@
+package hu.project.groupproject.entities;
+
+public class OpeningHoursUsual {
+    //business id
+    //day of week
+    //open from
+    //open till
+    //valid from date 
+    //valid till date
+    
+
+}
