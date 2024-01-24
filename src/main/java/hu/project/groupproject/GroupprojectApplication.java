1284c36750bdd7b038a99d25ef7e4d816f824700
MrBrown16 <morabarna16@gmail.com>
2024-01-24 22:49:18 +0100

    worked on auth not working

--- src/main/java/hu/project/groupproject/GroupprojectApplication.java
+++ src/main/java/hu/project/groupproject/GroupprojectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// @ComponentScan(basePackages = "hu.project.groupproject")
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "hu.project.groupproject")
 public class GroupprojectApplication {
@@ -21,18 +20,3 @@ public class GroupprojectApplication {
 }
 
 
-
-
-// try(AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext()){
-// // new AnnotationConfigWebApplicationContext(GroupprojectApplication.class,RepoConfig.class)){
-// 	// SpringApplication.run(GroupprojectApplication.class, args);
-// 	ctx.scan("hu.project.groupproject");
-// 	ctx.register(RepoConfig.class);
-// 	ctx.refresh();
-// 	MyService s = ctx.getBean(MyService.class);
-// 	s.doWork();
-// }
-// AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(GroupprojectApplication.class, RepoConfig.class);
-// // SpringApplication.run(GroupprojectApplication.class, args);
-// MyService s = ctx.getBean(MyService.class);
-// s.doWork();
--- src/main/java/hu/project/groupproject/controllers/MainController.java
+++ src/main/java/hu/project/groupproject/controllers/MainController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class MainController {
     
-    // @GetMapping
-    // public void setUp(){
-    // }
+    @GetMapping
+    public String setUp(){
+        return "SUCCESSFUL LOGIN";
+    }
 }
