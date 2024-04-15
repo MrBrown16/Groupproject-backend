346932f0b1dbdac1f196cc8424135f8252deefe0
MrBrown16 <morabarna16@gmail.com>
2024-04-15 23:17:23 +0200

    the beggining of role management

--- authorization-server/src/main/java/hu/project/groupproject/authorizationserver/controllers/UserController.java
+++ authorization-server/src/main/java/hu/project/groupproject/authorizationserver/controllers/UserController.java
@@ -1,6 +1,8 @@
 package hu.project.groupproject.authorizationserver.controllers;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +10,14 @@ import hu.project.groupproject.authorizationserver.dtos.UserDTOPublic;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -44,6 +48,34 @@ public class UserController {
         manager.createUser(newUser);        
         return true;
     }
+
+    // @PutMapping("/update")
+    // public Boolean updateUser(@AuthenticationPrincipal User user,@RequestBody Map<String,String> usermap, Set<String> roles) {
+    //     String userName = usermap.get("userName"); //the user to be set
+    //     boolean admin = roles.contains("ADMIN");
+    //     boolean orgAdmin = roles.contains("ORG_ADMIN");
+    //     Set<String> toBeRoles = new HashSet<>();
+    //     toBeRoles.add("USER");
+    //     if (user.getAuthorities().contains(new SimpleGrantedAuthority("ORG_ADMIN")) || user.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
+    //         UserDetails oldUser = manager.loadUserByUsername(userName);
+    //         if(orgAdmin && !oldUser.getAuthorities().contains(new SimpleGrantedAuthority("ORG_ADMIN"))){
+    //             toBeRoles.add("ORG_ADMIN");
+    //         }
+    //         if(admin && !oldUser.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))){
+    //             toBeRoles.add("ORG_ADMIN");
+    //         }
+    //     }
+    //     // if (orgAdmin && user.getAuthorities().contains(new SimpleGrantedAuthority("ORG_ADMIN"))) {
+    //     // }
+    //     // if (admin && user.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
+    //     //     toBeRoles.add("ADMIN");
+    //     // }
+    //     // user.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"));
+    //     UserDetails updateUser = User.builder().username(userName)
+    //             .roles((String[])toBeRoles.toArray()).build();
+    //     manager.updateUser(updateUser);        
+    //     return true;
+    // }
     
 
 
